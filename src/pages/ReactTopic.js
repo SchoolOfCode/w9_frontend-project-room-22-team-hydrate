3a93e91e7ea443ef2556ea5322085f844275c731
alicialowe <hohpascal@outlook.com>
2022-06-23 08:18:06 +0100

    all topics now working - buttons on homepage navigate to individvigate to individual topic pages and render document and video links under separatnks under separate headings

--- src/pages/ReactTopic.js
+++ src/pages/ReactTopic.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 
+import List from "../components/List/List";
+import VidList from "../components/VidList/VidList";
 
 function ReactTopic() {
+    
+    useEffect( () => {
+        fetchItems();
+    }, []);
+
+    const [topicState, setTopicState] = useState([]);
+
+    const fetchItems = async () => {
+        const response = await fetch("http://localhost:3000/resources/React");
+        const data = await response.json();
+        //console.log(data);
+        setTopicState(data.payload);
+    };
+
+    //console.log(cssState);
+    
     let navigate = useNavigate();
     return (
         <>
-            <h1> React Boosters </h1>
+            <h2> Here are some React Boosters </h2>
+            <h3>Reading Resources</h3>
+            <List topicState={topicState}/>
+            <h3>Video Resources</h3>
+            <VidList topicState={topicState}/>
             <button onClick={() => {navigate("/");}}>Back to Home</button>
         </>
     )
--- src/pages/UiUxTopic.js
+++ src/pages/UiUxTopic.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 
+import List from "../components/List/List";
+import VidList from "../components/VidList/VidList";
+
 function UiUxTopic() {
+    
+    useEffect( () => {
+        fetchItems();
+    }, []);
+
+    const [topicState, setTopicState] = useState([]);
+
+    const fetchItems = async () => {
+        const response = await fetch("http://localhost:3000/resources/UXUI");
+        const data = await response.json();
+        //console.log(data);
+        setTopicState(data.payload);
+    };
+
+    //console.log(cssState);
+    
     let navigate = useNavigate();
     return (
         <>
-            <h1> UI / UX Boosters </h1>
+            <h2> Here are some UI / UX Boosters </h2>
+            <h3>Reading Resources</h3>
+            <List topicState={topicState}/>
+            <h3>Video Resources</h3>
+            <VidList topicState={topicState}/>
             <button onClick={() => {navigate("/");}}>Back to Home</button>
         </>    
     )
