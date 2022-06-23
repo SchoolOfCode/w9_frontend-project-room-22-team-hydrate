0b8de44b9f1ab8a451d78a03d0f59f563aea9a29
alicialowe <hohpascal@outlook.com>
2022-06-23 19:04:25 +0100

    error page updated

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import CatDrum from "../assets/CatDrum.gif";
 
 function ErrorPage() {
     let navigate = useNavigate();
     return (
-        <>
-            <div> OOPS, You've encountered an error....please go back you plum </div>
-            <img src="https://d.newsweek.com/en/full/1920025/cat-its-mouth-open.jpg?w=1600&h=1600&q=88&f=b7a44663e082b8041129616b6b73328d"/>
-            <button onClick={() => {navigate("/");}}>Back to Home</button>
+        <> 
+            <h1> Impromptu Dance Party! </h1>
+            <h2>This Booster isn't available yet - but this cat is bopping to some bongos, so there's that</h2>
+            <div>
+            <img src={CatDrum}/>
+            </div>
+            <button className="back-button" onClick={() => {navigate("/");}}>Back to Home</button>
         </>
     )
 }
