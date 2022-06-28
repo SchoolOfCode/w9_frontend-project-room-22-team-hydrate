7eee6f8c873e24dacb1097599dcb796a983d6dfc
alicialowe <hohpascal@outlook.com>
2022-06-28 16:16:48 +0100

    updated List/VidList and ListItem/VidListItem component files ro improve readability/spacing, remove old comments, remove trial code that was commented out.  summary comment added to function.  tested functionality in browser and all working ok

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,39 +1,11 @@
 import React from "react";
 
-
-//create a function
-    //needs to be async as awaiting fetch 
-    //needs to render ListItems as lis 
-        //needs to map through
-        //these are coming in from the backend response
+//uses props to formulate list items as links
 function ListItem({docsLink, description}) {
 
     return (
-        <>
-         {/* <li>{vidLink}  {docsLink} {description}</li> */}
-         {/* <li><a href={vidLink}>VideoLink</a></li>
-         <li>{description}</li> */}
          <li className="link"><a href={docsLink} target="_blank" rel="noopener noreferrer">{description}</a></li>
-         </>
     );
 }
 
-
-
-
 export default ListItem;
-
-
-/*     <table >
-            <tr>
-                <th>Video Link</th>
-                <th>Docs Link</th>
-                <th>Description</th>
-            </tr>
-            <tr>
-                <th>{vidLink}</th>
-                <th>{docsLink}</th>
-                <th>{description}</th>
-            </tr>
-         </table>
-         */
--- src/components/VidListItem/VidListItem.js
+++ src/components/VidListItem/VidListItem.js
@@ -1,24 +1,11 @@
 import React from "react";
 
-
-//create a function
-    //needs to be async as awaiting fetch 
-    //needs to render ListItems as lis 
-        //needs to map through
-        //these are coming in from the backend response
+//uses props to formulate list items as links
 function VidListItem({vidLink, description}) {
 
     return (
-        <>
-         {/* <li>{vidLink}  {docsLink} {description}</li> */}
-         {/* <li><a href={vidLink}>VideoLink</a></li>
-         <li>{description}</li> */}
-         <li className="link"><a href={vidLink}>{description}</a></li>
-         </>
+         <li className="link"><a href={vidLink} target="_blank" rel="noopener noreferrer">{description}</a></li>
     );
 }
-
-
-
 
 export default VidListItem;
