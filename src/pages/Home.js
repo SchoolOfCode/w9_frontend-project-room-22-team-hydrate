7506879d84a3d82ea5bb522d3b1719f85042dbb1
alicialowe <hohpascal@outlook.com>
2022-06-28 14:28:11 +0100

    renamed all asset files following same convention, and updated where imported and used as props in Home.js and Error.js.  all functionality and styling still working correctly on app in browser

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import BootcampBoosterLogo from "../assets/BootcampBoosterLogo.png";
-// import HtmlLogo from "../assets/html.png";
-import HtmlCorrect from "../assets/HtmlCorrect.png"
-import cssLogo from "../assets/css.png";
-import reactLogo from "../assets/react.png";
-import uiuxLogo from "../assets/uiux.png";
+import HtmlButton from "../assets/HtmlButton.png"
+import CssButton from "../assets/CssButton.png";
+import ReactButton from "../assets/ReactButton.png";
+import UiuxButton from "../assets/UiuxButton.png";
 
 function Home() {
     let navigate = useNavigate();
@@ -14,12 +13,12 @@ function Home() {
         <img className="logo" src={BootcampBoosterLogo} alt="BootcampBoosterLogo" />
         <h2>What would you like a boost on today?</h2>
         <div>
-            <button className="topic-button" onClick={() => {navigate("/html");}}><img className="topic-pic" src={HtmlCorrect} height ="239" width="373" /></button>
-            <button className="topic-button" onClick={() => {navigate("/css");}}><img className="topic-pic" src={cssLogo} height ="239" width="373" /></button>
+            <button className="topic-button" onClick={() => {navigate("/html");}}><img className="topic-pic" src={HtmlButton} height ="239" width="373" /></button>
+            <button className="topic-button" onClick={() => {navigate("/css");}}><img className="topic-pic" src={CssButton} height ="239" width="373" /></button>
         </div>
         <div>
-            <button className="topic-button" onClick={() => {navigate("/react");}}><img className="topic-pic" src={reactLogo} height ="239" width="373" /></button>
-            <button className="topic-button" onClick={() => {navigate("/uiux");}}><img className="topic-pic" src={uiuxLogo} height ="239" width="373" /></button>
+            <button className="topic-button" onClick={() => {navigate("/react");}}><img className="topic-pic" src={ReactButton} height ="239" width="373" /></button>
+            <button className="topic-button" onClick={() => {navigate("/uiux");}}><img className="topic-pic" src={UiuxButton} height ="239" width="373" /></button>
         </div>
         </>
     )
